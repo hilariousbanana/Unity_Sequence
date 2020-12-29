b45f1c204f1cf38c03c5b4343ce3fb2249e0df1a
hilariousbanana <62584335+hilariousbanana@users.noreply.github.com>
2020-12-30 04:00:33 +0900

    [Add] Database Settlement

---
+++ Untitled/Assets/Scripts/01.Database/ClearRecord.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ClearRecord
+{
+    public string PlayerName;
+    public int Minutes;
+    public int Seconds;
+    public int MilliSeconds;
+
+    public ClearRecord(string _playerName, int _minutes, int _seconds, int _milliSeconds)
+    {
+        PlayerName = _playerName;
+        Minutes = _minutes;
+        Seconds = _seconds;
+        MilliSeconds = _milliSeconds;
+    }
+}
---
+++ Untitled/Assets/Scripts/01.Database/Data.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Data
+{
+    public List<Quest> QuestList = new List<Quest>();
+    public List<ClearRecord> RecordList = new List<ClearRecord>();
+
+    public void AddQuestList()
+    {
+        QuestList.Add(new Quest(Quest.STAGE.Stage1, 10, 0, 0, 0));
+        QuestList.Add(new Quest(Quest.STAGE.Stage2, 10, 5, 0, 0));
+        QuestList.Add(new Quest(Quest.STAGE.Stage3, 0, 3, 3, 3));
+        QuestList.Add(new Quest(Quest.STAGE.Final, 20, 0, 0, 0));
+    }
+}
